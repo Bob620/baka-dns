2de874f7a361c791667d33a814ddb8148024ca35
bob620 <bruder.kraft225@gmail.com>
2021-04-19 02:42:35 -0500

    chore(do) cleanup caching of dns entries

--- dnshandler.go
+++ dnshandler.go
@@ -18,7 +18,7 @@ func MakeDNSHandler(redisPool *RedisPool, dnsPool *pool.Pool, localCache *cache.
 	return &DnsHandler{redisPool, dnsPool, localCache}
 }
 
-func (handler DnsHandler) cache(name string, typ uint16) {
+func (handler DnsHandler) cache(name string, typ uint16, tangent bool) *dns.Msg {
 	result, onlyCname := handler.localCache.Get(dns.Name(name), dns.Type(typ))
 	if result == nil || onlyCname {
 		dnsRes, source, _ := handler.dnsPool.Do(pool.Message{Name: name, Type: typ})
@@ -29,17 +29,22 @@ func (handler DnsHandler) cache(name string, typ uint16) {
 			if dnsRes.Rcode == dns.RcodeSuccess {
 				go func() {
 					if len(dnsRes.Answer) > 0 {
-						handler.localCache.Set(dns.Name(name), dnsRes.Answer, true)
+						handler.localCache.Set(dns.Name(name), dnsRes.Answer, tangent)
 					}
 				}()
+
+				return dnsRes
 			}
 		}
 	}
+
+	return nil
 }
 
 func (handler DnsHandler) Do(question *dns.Question) (*dns.Msg, error) {
 	go fmt.Printf("Searching for %s with record type %s\n", question.Name, dns.TypeToString[question.Qtype])
 
+	// Local cache lookup
 	result, onlyCname := handler.localCache.Get(dns.Name(question.Name), dns.Type(question.Qtype))
 
 	if result != nil && !onlyCname {
@@ -47,39 +52,22 @@ func (handler DnsHandler) Do(question *dns.Question) (*dns.Msg, error) {
 		return &dns.Msg{Answer: result}, nil
 	}
 
-	var dnsRes *dns.Msg
-	dnsRes, source, _ := handler.dnsPool.Do(pool.Message{Name: question.Name, Type: question.Qtype})
-
+	// Tangent queries
 	go func(name string, qType uint16) {
-		switch qType {
-		case dns.TypeA:
-			fallthrough
-		case dns.TypeAAAA:
-			fallthrough
-		case dns.TypeCNAME:
-			fallthrough
-		case dns.TypeMX:
-			fallthrough
-		case dns.TypeNS:
-			fallthrough
-		case dns.TypeTXT:
-			go handler.cache(name, qType)
-		}
+		go handler.cache(name, dns.TypeA, true)
+		go handler.cache(name, dns.TypeAAAA, true)
+		go handler.cache(name, dns.TypeCNAME, true)
+		go handler.cache(name, dns.TypeMX, true)
+		go handler.cache(name, dns.TypeNS, true)
+		go handler.cache(name, dns.TypeTXT, true)
 	}(question.Name, question.Qtype)
 
-	// Catch when all of the upstream resolvers fail
+	// Query upstream DNS
+	dnsRes := handler.cache(question.Name, question.Qtype, false)
+
+	// Catch if all of the upstream resolvers fail and return an error
 	if dnsRes != nil {
-		go fmt.Printf("%s (T:%s) found in %s (P:%d) with %d answers\n", question.Name, dns.TypeToString[question.Qtype], source.Name, source.Priority, len(dnsRes.Answer))
-
-		if dnsRes.Rcode == dns.RcodeSuccess {
-			go func() {
-				if len(dnsRes.Answer) > 0 {
-					handler.localCache.Set(dns.Name(question.Name), dnsRes.Answer, false)
-				}
-			}()
-
-			return dnsRes, nil
-		}
+		return dnsRes, nil
 	}
 
 	return nil, errors.New("nxdomain")
